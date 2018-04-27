069f2a59422358570d81195beae2871fe11a36a4
Miguel R. Laher <iamigslaher@gmail.com>
2018-04-28 01:38:30 +0800

    made a menu for topics

---
+++ app/src/main/java/com/example/laher/learnfractions/RecyclerViewAdapter.java
@@ -0,0 +1,85 @@
+package com.example.laher.learnfractions;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by Laher on 4/28/2018.
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter (Context context ,ArrayList<String> imageNames, ArrayList<String> images){
+        mImageNames = imageNames;
+        mImages = images;
+        mContext = context;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_list_topic, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImages.get(position))
+                .into(holder.imgTopic);
+
+        holder.txtTopicName.setText(mImageNames.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on: " + mImageNames.get(position));
+
+                Toast.makeText(mContext, mImageNames.get(position), Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        CircleImageView imgTopic;
+        TextView txtTopicName;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imgTopic = itemView.findViewById(R.id.imgTopic);
+            txtTopicName = itemView.findViewById(R.id.txtTopic);
+            parentLayout = itemView.findViewById(R.id.parent_layout_topic);
+        }
+    }
+}
--- app/src/main/java/com/example/laher/learnfractions/TopicsMenuActivity.java
+++ app/src/main/java/com/example/laher/learnfractions/TopicsMenuActivity.java
@@ -2,12 +2,71 @@ package com.example.laher.learnfractions;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class TopicsMenuActivity extends AppCompatActivity {
+    private static final String TAG = "TopicsMenuActivity";
+
+    //vars
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topics_menu);
+        Log.d(TAG, "onCreate: started.");
+
+        initImageBitmaps();
+    }
+
+    private void initImageBitmaps(){
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+        mImageUrls.add("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Salto_del_Angel-Canaima-Venezuela08.JPG/1200px-Salto_del_Angel-Canaima-Venezuela08.JPG");
+        mNames.add("waterfall");
+
+        mImageUrls.add("https://upload.wikimedia.org/wikipedia/commons/b/bc/Metolius_River_near_Wizard_Falls.jpg");
+        mNames.add("river");
+
+        mImageUrls.add("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Salto_del_Angel-Canaima-Venezuela08.JPG/1200px-Salto_del_Angel-Canaima-Venezuela08.JPG");
+        mNames.add("waterfall");
+
+        mImageUrls.add("https://upload.wikimedia.org/wikipedia/commons/b/bc/Metolius_River_near_Wizard_Falls.jpg");
+        mNames.add("river");
+
+        mImageUrls.add("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Salto_del_Angel-Canaima-Venezuela08.JPG/1200px-Salto_del_Angel-Canaima-Venezuela08.JPG");
+        mNames.add("waterfall");
+
+        mImageUrls.add("https://upload.wikimedia.org/wikipedia/commons/b/bc/Metolius_River_near_Wizard_Falls.jpg");
+        mNames.add("river");
+
+        mImageUrls.add("https://upload.wikimedia.org/wikipedia/commons/b/bc/Metolius_River_near_Wizard_Falls.jpg");
+        mNames.add("river");
+
+        mImageUrls.add("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Salto_del_Angel-Canaima-Venezuela08.JPG/1200px-Salto_del_Angel-Canaima-Venezuela08.JPG");
+        mNames.add("waterfall");
+
+        mImageUrls.add("https://upload.wikimedia.org/wikipedia/commons/b/bc/Metolius_River_near_Wizard_Falls.jpg");
+        mNames.add("river");
+
+        mImageUrls.add("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Salto_del_Angel-Canaima-Venezuela08.JPG/1200px-Salto_del_Angel-Canaima-Venezuela08.JPG");
+        mNames.add("waterfall");
+
+        mImageUrls.add("https://upload.wikimedia.org/wikipedia/commons/b/bc/Metolius_River_near_Wizard_Falls.jpg");
+        mNames.add("river");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: init recyclerview.");
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_topics);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mNames, mImageUrls);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
